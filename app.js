78fea616783f7984a7023fa48ebbc490b6f23726
Caskito1 <caskito90@gmail.com>
2022-09-19 20:29:50 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector("#mobile-menu")
+const menuLinks = document.querySelector(".navbar__menu")
+const navLogo = document.querySelector(".navbar__logo")
+const body = document.querySelector ("body")
+
+const mobileMenu = () => {
+    menu.classList.toggle ("is-active")
+    menuLinks.classList.toggle ("active")
+    body.classList.toggle ("active")
+      
+}
+
+menu.addEventListener("click",mobileMenu);
